73b373d934038660808485c9d9498716a55d94d6
RostisLAVE01 <moskalets_03@mail.ru>
2024-09-23 00:57:05 +0300

    Created_several_examples_of_filling

---
+++ main.kt
@@ -0,0 +1,38 @@
+fun main() {
+    val example_1 = Student(
+        id = 1,
+        surname = "Фолгимштейн",
+        name = "Федор",
+        patronymic = "Крузинштейн",
+        phone = "+7-989-345-34-67",
+        telegram = "@ROOMdoom",
+        email = "RoomDoom@mail.ru",
+        git = "https://github.com/Fedor"
+    )
+
+    val example_2 = Student(
+        id = 2,
+        surname = "Шаповалова",
+        name = "Карина",
+        phone = "+7-656-134-67-89"
+    )
+
+    val example_3 = Student(
+        id = 3,
+        surname = "Зайцев",
+        name = "Иехонтий",
+        email = "zoom@example.com"
+    )
+
+    val example_4 = Student(
+        id = 4,
+        surname = "Смурыгин",
+        name = "Яхонт",
+        telegram = "@yakhont"
+    )
+
+    println(example_1)
+    println(example_2)
+    println(example_3)
+    println(example_4)
+}
