51d30b11dc2bd1a1ca3152e50809e929b7b07333
RostisLAVE01 <moskalets_03@mail.ru>
2024-10-27 01:24:14 +0300

    implemented_methods_select,get_selected_2.4

--- Datalist.kt
+++ Datalist.kt
@@ -1,4 +1,38 @@
 class Datalist(val data: Array<Any>)
 {
+    private val select = mutableListOf<Int>()
+
+    fun select(num:Int)
+    {
+        if(num < data.size)
+        {
+            select.add(num)
+        }
+    }
+
+    fun get_selected() : MutableList<Int>
+    {
+        return select
+    }
+
+    fun get_names(id:Int): Array<String>
+    {
+        var name = data[id]!!::class.java.declaredFields.map { it.name }.toTypedArray()
+        name[0] = id.toString()
+        return name
+    }
+
+    fun get_data(): DataTable
+    {
+        var attr = mutableListOf<List<Any>>()
+        for (id in select)
+        {
+            var arg = mutableListOf<Any>(id)
+            arg.add(data[id]!!::class.java.declaredFields.map { it }.toList())
+            attr.add(arg)
+
+        }
+        return DataTable(attr)
+    }
 
 }
